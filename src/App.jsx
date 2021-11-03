3d6202e66b693cf36f6a5e4f39bb3edd2d1bdf13
903958734@qq.com <903958734@qq.com>
2021-11-03 23:02:38 +0800

    利用connect减少render:加入打印日志，跟踪每次input值改变时触发render的组件

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,22 @@ const App =() => {
         </appContext.Provider>
     )
 }
-const BigSon = () => <section>大儿子<User/></section>
-const SecondSon = () => <section>二儿子<UserModifier x={'d'}>hhh</UserModifier></section>
-const SmallSon = () => <section>小儿子</section>
+//()=>xxx  句式会自动return  ()=>{}句式需要在花括号里写return
+const BigSon = () => {
+    console.log('大儿子执行了' + Math.random())
+   return <section>大儿子<User/></section>
+}
+const SecondSon = () => {
+    console.log('二儿子执行了' + Math.random())
+    return <section>二儿子<UserModifier x={'d'}>hhh</UserModifier></section>
+}
+const SmallSon = () => {
+    console.log('小儿子执行了' + Math.random())
+    return <section>小儿子</section>
+}
 
 const User = () =>{
+    console.log('user执行了' + Math.random())
     const {appState} = useContext(appContext)
     return(<div>{appState.user.name}</div>)
 }
@@ -46,6 +57,7 @@ const connect = (Component) =>{
 
 }
 const UserModifier = connect(({dispatch,state,children}) => {  //解构赋值，此时的pros为:{x:'d',children:'hhh',dispatch:(action)=>{},state:{user:{name:'bobojier',age:18}}}
+    console.log('userModifier执行了' + Math.random())
     const onChange = (e) =>{
         dispatch({type:"updateState",payload:{name:e.target.value}})
     }
