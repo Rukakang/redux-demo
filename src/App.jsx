9a10dd710126362443a428f81d0408035e49ab1b
903958734@qq.com <903958734@qq.com>
2021-11-03 22:42:38 +0800

    connect的来历：UserModifier的其他props和children也支持通过wrapper透传给_UserModifier组件

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App =() => {
     )
 }
 const BigSon = () => <section>大儿子<User/></section>
-const SecondSon = () => <section>二儿子<UserModifier/></section>
+const SecondSon = () => <section>二儿子<UserModifier x={'d'}>hhh</UserModifier></section>
 const SmallSon = () => <section>小儿子</section>
 
 const User = () =>{
@@ -34,23 +34,24 @@ const reducer = (state,{type,payload})=>{
     }
 }
 
-const _UserModifier = ({dispatch,state}) => {
+
+const createWrapper = (Component) =>{
+    const Wrapper =(props)=>{
+        const {appState,setAppState} = useContext(appContext)
+        const dispatch =(action)=>{
+            setAppState(reducer(appState,action))
+        }
+        return <Component {...props} dispatch={dispatch} state={appState}/>
+    }
+    return Wrapper
+}
+const _UserModifier = ({dispatch,state,children}) => {  //解构赋值，此时的pros为:{x:'d',children:'hhh',dispatch:(action)=>{},state:{user:{name:'bobojier',age:18}}}
     const onChange = (e) =>{
         dispatch({type:"updateState",payload:{name:e.target.value}})
     }
     return(
         <input value={state.user.name} onChange={onChange}/>
     )
-}
-const createWrapper = (Component) =>{
-    const Wrapper =()=>{
-        const {appState,setAppState} = useContext(appContext)
-        const dispatch =(action)=>{
-            setAppState(reducer(appState,action))
-        }
-        return <Component dispatch={dispatch} state={appState}/>
-    }
-    return Wrapper
 }
 const UserModifier = createWrapper(_UserModifier)
 
