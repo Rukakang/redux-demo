c0235619670b89b78ef02462107526e34f4df296
903958734@qq.com <903958734@qq.com>
2021-11-07 22:26:16 +0800

    Redux乍现：把redux抽离出来

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,5 @@
-import React,{useState,useContext,useMemo,useEffect} from 'react';
-
-const appContext = React.createContext(null);
-const store = {
-    state:{
-        user:{
-            name:'bobojier',
-            age:18
-        }
-    },
-    setState(newState){   //store的setState不是hooks的setState
-        store.state = newState
-        store.linsterners.map(fn=>fn(store.state)) //setState的时候，订阅了store变化的监听者都进行函数回调
-    },
-    linsterners:[],
-    subscribe(fn){
-        //store发生变化时要执行的函数放入数组里
-        store.linsterners.push(fn)
-        return ()=>{  //return的函数可以取消订阅
-            const index = store.linsterners.indexOf(fn)
-            store.linsterners.splice(index,1)
-        }
-    }
-}
-
+import React from 'react';
+import {store,appContext,connect} from  './redux'
 const App =() => {
     //const [appState,setAppState] = useState({user:{name:'bobojier',age:18}})
    // const contextValue ={appState,setAppState}
@@ -47,40 +24,6 @@ const SecondSon = () => {
 const SmallSon = () => {
     console.log('小儿子执行了' + Math.random())
     return <section>小儿子</section>
-}
-
-
-const reducer = (state,{type,payload})=>{
-    if(type === "updateState"){
-        return {
-            ...state,
-            user:{
-                ...state.user,
-                ...payload
-            }
-        }
-    }else{
-        return state
-    }
-}
-
-const connect = (Component) =>{
-    return (props)=>{  //接受一个组件，返回一个组件
-        const {state,setState} = useContext(appContext)
-        const[,update] = useState({})
-
-        //订阅store
-        useEffect(()=>{
-            store.subscribe(()=>{
-                update({})
-            })
-        },[])
-
-        const dispatch =(action)=>{
-            setState(reducer(state,action))
-        }
-        return <Component {...props} dispatch={dispatch} state={state}/> //返回的组件中又引入了子组件，子组件的props是包裹子组件的组件透传过来的
-    }
 }
 
 const UserModifier = connect(({dispatch,state,children}) => {  //解构赋值，此时的pros为:{x:'d',children:'hhh',dispatch:(action)=>{},state:{user:{name:'bobojier',age:18}}}
---
+++ src/redux.jsx
@@ -0,0 +1,57 @@
+import React,{useState,useContext,useEffect} from 'react';
+
+export const appContext = React.createContext(null);
+
+export const store = {
+    state:{
+        user:{
+            name:'bobojier',
+            age:18
+        }
+    },
+    setState(newState){   //store的setState不是hooks的setState
+        store.state = newState
+        store.linsterners.map(fn=>fn(store.state)) //setState的时候，订阅了store变化的监听者都进行函数回调
+    },
+    linsterners:[],
+    subscribe(fn){
+        //store发生变化时要执行的函数放入数组里
+        store.linsterners.push(fn)
+        return ()=>{  //return的函数可以取消订阅
+            const index = store.linsterners.indexOf(fn)
+            store.linsterners.splice(index,1)
+        }
+    }
+}
+export const reducer = (state,{type,payload})=>{
+    if(type === "updateState"){
+        return {
+            ...state,
+            user:{
+                ...state.user,
+                ...payload
+            }
+        }
+    }else{
+        return state
+    }
+}
+
+export const connect = (Component) =>{
+    return (props)=>{  //接受一个组件，返回一个组件
+        const {state,setState} = useContext(appContext)
+        const[,update] = useState({})
+
+        //订阅store
+        useEffect(()=>{
+            store.subscribe(()=>{
+                update({})
+            })
+        },[])
+
+        const dispatch =(action)=>{
+            setState(reducer(state,action))
+        }
+        return <Component {...props} dispatch={dispatch} state={state}/> //返回的组件中又引入了子组件，子组件的props是包裹子组件的组件透传过来的
+    }
+}
