b9f1ed9c33fe4c16350bcbd0cf4c05f2a1745956
903958734@qq.com <903958734@qq.com>
2021-11-07 16:12:13 +0800

    使用connect减少render次数,用到state的组件添加订阅，state变化的时候，依赖state的组件相应更新

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React,{useState,useContext,useMemo} from 'react';
+import React,{useState,useContext,useMemo,useEffect} from 'react';
 
 const appContext = React.createContext(null);
 const store = {
@@ -10,6 +10,16 @@ const store = {
     },
     setState(newState){   //store的setState不是hooks的setState
         store.state = newState
+        store.linsterners.map(fn=>fn(store.state)) //setState的时候，订阅了store变化的监听者都进行函数回调
+    },
+    linsterners:[],
+    subscribe(fn){
+        //store发生变化时要执行的函数放入数组里
+        store.linsterners.push(fn)
+        return ()=>{  //return的函数可以取消订阅
+            const index = store.linsterners.indexOf(fn)
+            store.linsterners.splice(index,1)
+        }
     }
 }
 
@@ -39,11 +49,7 @@ const SmallSon = () => {
     return <section>小儿子</section>
 }
 
-const User = () =>{
-    console.log('user执行了' + Math.random())
-    const {state} = useContext(appContext)
-    return(<div>{state.user.name}</div>)
-}
+
 const reducer = (state,{type,payload})=>{
     if(type === "updateState"){
         return {
@@ -59,16 +65,24 @@ const reducer = (state,{type,payload})=>{
 }
 
 const connect = (Component) =>{
-    return (props)=>{
+    return (props)=>{  //接受一个组件，返回一个组件
         const {state,setState} = useContext(appContext)
         const[,update] = useState({})
+
+        //订阅store
+        useEffect(()=>{
+            store.subscribe(()=>{
+                update({})
+            })
+        },[])
+
         const dispatch =(action)=>{
             setState(reducer(state,action))
-            update({}) //当调用store的setState的时候，触发强制更新
         }
-        return <Component {...props} dispatch={dispatch} state={state}/>
+        return <Component {...props} dispatch={dispatch} state={state}/> //返回的组件中又引入了子组件，子组件的props是包裹子组件的组件透传过来的
     }
 }
+
 const UserModifier = connect(({dispatch,state,children}) => {  //解构赋值，此时的pros为:{x:'d',children:'hhh',dispatch:(action)=>{},state:{user:{name:'bobojier',age:18}}}
     console.log('userModifier执行了' + Math.random())
     const onChange = (e) =>{
@@ -78,6 +92,11 @@ const UserModifier = connect(({dispatch,state,children}) => {  //解构赋值，
         <input value={state.user.name} onChange={onChange}/>
     )
 })
-
+const User =connect(
+    ({state}) =>{
+        console.log('user执行了' + Math.random())
+        return(<div>{state.user.name}</div>)
+    }
+)
 
 export default App;
