37aaa6f8ccf785fe9a38b303986bdbf302c7fa39
903958734@qq.com <903958734@qq.com>
2021-11-14 14:21:33 +0800

    代码简化

--- src/redux.jsx
+++ src/redux.jsx
@@ -57,16 +57,16 @@ export const connect = (selector) => (Component) =>{  //柯里化
         const[,update] = useState({})
         const data =selector ? selector(state) :{state:state}  //如果传了state就是局部的state,没传就是全局的state
         //订阅store
-        useEffect(()=>{
+        useEffect(()=>
             //useEffect有return的话，会在组件销毁之前执行；如果有多次渲染，就会在执行下一个effect之前，上一个effect被清除的时候执行return的函数
-            return store.subscribe(()=>{   //第一次执行时，会订阅store(就是传一个回调函数给store先存起来),当数据变化后，会先触发dispatch改变数据，然后再执行这个回调函数
+           store.subscribe(()=>{   //第一次执行时，会订阅store(就是传一个回调函数给store先存起来),当数据变化后，会先触发dispatch改变数据，然后再执行这个回调函数
                 const newData = selector? selector(store.state) :{state:store.state}
                 if(changed(data,newData)){
                     console.log("changed")
                     update({})
                 }
             })
-        },[selector,state]) //如果不取消订阅，当加上state依赖时，会造成重复渲染，比如当二儿子改变两次时，大儿子和二儿子都会渲染两遍，因为每次state变化时，重复订阅了大儿子和二儿子，store.lisenters数组被push了多余的重复回调函数
+        ,[selector]) //如果不取消订阅，当加上state依赖时，会造成重复渲染，比如当二儿子改变两次时，大儿子和二儿子都会渲染两遍，因为每次state变化时，重复订阅了大儿子和二儿子，store.lisenters数组被push了多余的重复回调函数
 
         const dispatch =(action)=>{
             setState(reducer(state,action))
